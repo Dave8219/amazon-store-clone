2bc946c6c3470be37d4f9575c1a97916b1cfa3c6
DaveJS <dgarcia123david@gmail.com>
2024-08-05 12:53:49 -0500

    Generate HTML for the order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,7 +70,6 @@ function updateCartQuantity() {
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
-
     document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         button.addEventListener('click', () => {
         const productId = button.dataset.productId;
