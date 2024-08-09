4a6b150d6a0e015129bc6d15256e8c11cab3ec50
DaveJS <dgarcia123david@gmail.com>
2024-08-09 16:01:03 -0500

    Styled and added event listeners to update link and added the save link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -39,9 +39,13 @@ js-cart-item-container-${matchingProduct.id}">
             <span>
             Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
             Update
             </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary">Save</span>
+
+
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
             </span>
@@ -120,3 +124,14 @@ function updateCartQuantity() {
 
 
 updateCartQuantity();
+
+
+
+document.querySelectorAll('.js-update-link').forEach((link) => {
+link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.add('is-editing-quantity');
+});
+});
