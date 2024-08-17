faf9943a73ed9b473232f0961a63698c1fa15623
DaveJS <dgarcia123david@gmail.com>
2024-08-17 14:40:02 -0500

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -6,10 +6,11 @@ import {
     updateDeliveryOption 
     } 
     from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 const today = dayjs();
@@ -24,23 +25,12 @@ export function renderOrderSummary() {
     cart.forEach((cartItem) => {
 
     const productId = cartItem.productId;
-    let matchingProduct;
+    const matchingProduct = getProduct(productId);
 
-    products.forEach((product) => {
-    if (product.id === productId) {
-    matchingProduct = product;
-    }
-    });
-
+    
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-    if (option.id === deliveryOptionId) {
-    deliveryOption = option;
-    }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
 
     const today = dayjs();
@@ -215,6 +205,7 @@ export function renderOrderSummary() {
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
     });
     });
 }
