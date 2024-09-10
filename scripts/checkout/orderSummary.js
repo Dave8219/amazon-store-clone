f619a9b80eaafde5e6dae5206890204ea7c32e22
DaveJS <dgarcia123david@gmail.com>
2024-09-10 16:03:08 -0500

    Tested the delivery option updates in OrderSummaryTest.js

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -93,11 +93,12 @@ export function renderOrderSummary() {
     
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     
-    html += `<div class="delivery-option js-delivery-option"
+    //the matchingProduct.id and deliveryOption.id used together in a class are used to test. Same thing with the js-payment-summary-shipping and js-payment-summary-total in the paymentSummary.js file.  
+    html += `<div class="delivery-option js-delivery-option js-delivery-option-${matchingProduct.id}-${deliveryOption.id}"
     data-product-id="${matchingProduct.id}"
     data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio" ${isChecked ? 'checked' : '' }
-                class="delivery-option-input"
+                class="delivery-option-input js-delivery-option-input-${matchingProduct.id}-${deliveryOption.id}"
                 name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -40,18 +40,18 @@ expect(
 expect(
     document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
 
-    expect(
-        document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
+expect(
+    document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
 
-        expect(
-            document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+expect(
+    document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
 
-            expect(
-                document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball');
+expect(
+    document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball');
 
-                expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toContain('$10.90');
+expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toContain('$10.90');
 
-                expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toContain('$20.95');
+expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toContain('$20.95');
 });
 
 
@@ -79,4 +79,17 @@ expect(cart.length).toEqual(1);
 expect(cart[0].productId).toEqual(productId2);
 
 });
+
+it('it updates the delivery option', () => {
+document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+expect(document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked).toEqual(true);
+    
+expect(cart.length).toEqual(2);
+expect(cart[0].productId).toEqual(productId1);
+expect(cart[0].deliveryOptionId).toEqual('3');
+expect(document.querySelector('.js-payment-summary-shipping').innerText).toEqual('$14.98');
+expect(document.querySelector('.js-payment-summary-total').innerText).toEqual('$63.50');
 });
+});
+
