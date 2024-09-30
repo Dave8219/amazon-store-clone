da3074b67908ba8638cc365641017f30076a9b5e
DaveJS <dgarcia123david@gmail.com>
2024-09-30 16:09:53 -0500

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,18 +4,63 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import '../data/car.js';
+// import '../data/car.js';
+import {loadCart} from '../data/cart.js';
 
 
 
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {    
+        loadProducts(() => { 
+            resolve('value1');
+        });
+        }),
+        new Promise((resolve) => {
+            loadCart(() => {
+            resolve();
+            });
+        }) 
 
+]).then((values) => {
+    console.log(values);
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
-
 });
 
 
+
+
+/*
+new Promise((resolve) => {    
+loadProducts(() => { 
+    resolve('value1');
+});
+
+}).then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        loadCart(() => {
+        resolve();
+        });
+    });
+
+}).then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+/*callbacks, then renders the rest of the page. Too many callbacks creates nesting, which is messy.
+loadProducts(() => {
+    loadCart(() => {
+        renderCheckoutHeader();
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+});
+*/
 
