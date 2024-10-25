0cd10e21f0e329ad6b31db6131bf54404b2a79db
DaveJS <dgarcia123david@gmail.com>
2024-10-25 15:43:55 -0500

    made the "added to cart" message visible with the classList.add method in amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,7 @@ return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart js-added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -98,7 +98,9 @@ return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase
     document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 
-        
+const addedMessageTimeouts = {}; 
+
+
     function updateCartQuantity() {
     const cartQuantity = calculateCartQuantity();
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
@@ -138,6 +140,21 @@ return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase
 
             addToCart(productId);
             updateCartQuantity(); 
+
+            const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+            addedMessage.classList.add('added-to-cart-visible');
+
+const previousTimeoutId = addedMessageTimeouts[productId];
+
+if (previousTimeoutId) {
+clearTimeout(previousTimeoutId);
+}
+
+const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+}, 2000);
+
+addedMessageTimeouts[productId] = timeoutId;
 
                 });
                 });
