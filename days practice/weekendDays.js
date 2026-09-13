32fd544c584eac9672c0770b70b39c352e66f8bf
DaveJS <dgarcia123david@gmail.com>
2026-09-12 21:30:21 -0500

    deleted backend-practice and car.js

--- days practice/weekendDays.js
+++ days practice/weekendDays.js
@@ -1,9 +1,8 @@
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 //function renamed in dayjs.js as isSatSun
 export default function isWeekend(date) {
-    date = dayjs();
-    const dayOfWeek = date.format('dddd');
-    return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';  
-  }
+  date = dayjs();
+  const dayOfWeek = date.format("dddd");
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
+}
